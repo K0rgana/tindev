58d7a41c523668738083462ad78101698e23a190
k0rgana <anagrom1999@gmail.com>
2019-11-11 00:30:28 -0300

    criando metodos no controller dev

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,9 +1,43 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
 module.exports = {
-    store(req, res){
+    async index(req, res) {
+        const { user } = req.headers;
+
+        const loggedDev = await Dev.findById(user);
+
+        const users = await Dev.find({
+            $and: [
+                { _id: { $ne: user } },
+                { _id: { $nin: loggedDev.likes } },
+                { _id: { $nin: loggedDev.dislikes } },
+            ],
+        })
+
+        return res.json(users);
+    },
+
+    async store(req, res) {
         const { username } = req.body;
 
-        //time 45:00 
+        const userExists = await Dev.findOne({ user: username });
+
+        if(userExists){
+            return res.json(userExists);
+        }
         
-        return res.json({ ok: true });
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+
+        const { name, avatar_url, bio } = response.data;
+
+        const dev = await Dev.create({
+            name,
+            user: username,
+            avatar: avatar_url,
+            bio
+        })
+
+        return res.json(dev);
     }
 };
