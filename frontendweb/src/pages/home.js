d33e12d910e9b07a608d10d0b57f41304a1dfb5a
k0rgana <anagrom1999@gmail.com>
2019-11-12 20:44:18 -0300

    criando estrutura da home

--- frontendweb/src/pages/home.js
+++ frontendweb/src/pages/home.js
@@ -1,7 +1,78 @@
 import React from 'react';
 
-export default function Home() {
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+export default function Home({ match }) {
     return(
-        <h1> This is your home </h1>
-    );
-}
+        <div className="home-container">
+            <img src={logo} alt="Tindev logo"/>
+            <ul>
+                <li>
+                <img src="https://avatars3.githubusercontent.com/u/48961500?v=4" alt=""/>
+                <footer>
+                    <strong>Morgana Fernandes</strong>
+                    <p>a vida é uma life</p>
+                </footer>
+                <div className="buttons">
+                    <button type="button">
+                        <img src={dislike} alt="dislike"/>
+                    </button>
+                    <button type="button">
+                        <img src={like} alt="like"/>
+                    </button>
+                </div>
+                </li>
+
+                <li>
+                <img src="https://avatars3.githubusercontent.com/u/48961500?v=4" alt=""/>
+                <footer>
+                    <strong>Morgana Fernandes</strong>
+                    <p>a vida é uma life</p>
+                </footer>
+                <div className="buttons">
+                    <button type="button">
+                        <img src={dislike} alt="dislike"/>
+                    </button>
+                    <button type="button">
+                        <img src={like} alt="like"/>
+                    </button>
+                </div>
+                </li>
+
+                <li>
+                <img src="https://avatars3.githubusercontent.com/u/48961500?v=4" alt=""/>
+                <footer>
+                    <strong>Morgana Fernandes</strong>
+                    <p>a vida é uma life</p>
+                </footer>
+                <div className="buttons">
+                    <button type="button">
+                        <img src={dislike} alt="dislike"/>
+                    </button>
+                    <button type="button">
+                        <img src={like} alt="like"/>
+                    </button>
+                </div>
+                </li>
+                
+                <li>
+                <img src="https://avatars3.githubusercontent.com/u/48961500?v=4" alt=""/>
+                <footer>
+                    <strong>Morgana Fernandes</strong>
+                    <p>a vida é uma life</p>
+                </footer>
+                <div className="buttons">
+                    <button type="button">
+                        <img src={dislike} alt="dislike"/>
+                    </button>
+                    <button type="button">
+                        <img src={like} alt="like"/>
+                    </button>
+                </div>
+                </li>
+            </ul>
+        </div>
+    
+    )} 
