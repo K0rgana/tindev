97f08cbc7281bbc9cc181d6144192a240bbe9fa3
k0rgana <anagrom1999@gmail.com>
2019-11-12 17:16:06 -0300

    adicionando logo da aplicação

--- frontendweb/src/App.js
+++ frontendweb/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <h1>Hello K0rgana</h1>
+    <img src={logo} alt="Tindev logo"/>
   );
 }
 
