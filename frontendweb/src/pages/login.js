a7deb3ee24a762f1f7453927e3d74bf64bb1699f
k0rgana <anagrom1999@gmail.com>
2019-11-12 19:11:45 -0300

    modificando login para guardar variaveis

--- frontendweb/src/pages/login.js
+++ frontendweb/src/pages/login.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './login.css';
 import logo from '../assets/logo.svg';
 
-export default function Login() {
+export default function Login({ history }) {
+    const [username, setUsername] = useState('');
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+
+        console.log(username);
+        history.push('/home');
+    }
+
     return(
         <div className="login-container">
-
-            <form>
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="Tindev logo"/>
-                <input placeholder="Type your username from GitHub"/>
-                <button type="submit"> Join</button>
+                <input 
+                placeholder="Type your username from GitHub"
+                value={username}
+                onChange= {evt => setUsername(evt.target.value)}
+                />
+                <button type="submit">Join</button>
             </form>
-
         </div>
-        
-
     );
 }
