2faf14ef184152f14a61884b3612afc9e776e741
k0rgana <anagrom1999@gmail.com>
2019-11-12 22:56:27 -0300

    implementando likes e deslikes

--- frontendweb/src/pages/home.js
+++ frontendweb/src/pages/home.js
@@ -21,29 +21,47 @@ export default function Home({ match }) {
         }
         loadUsers();
     },[match.params.id]);
+
+    async function handleLike(id){
+        await api.post(`/devs/${id}/likes`, null, {
+            headers: { user: match.params.id },
+        })
+        setUsers(users.filter(user => user._id !== id));
+    }
+    async function handleDislike(id){
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers: { user: match.params.id},
+        })
+        setUsers(users.filter(user => user._id !== id));
+    }
+
     return(
         <div className="home-container">
             <img src={logo} alt="Tindev logo"/>
+            {users.length > 0 ? (
             <ul>
                 {users.map(user => (
                     <li key={user._id}>
-                        <img src={user.avatar} alt="{user.name}"/>
+                        <img src={user.avatar} alt={user.name}/>
                         <footer>
                             <strong>{user.name}</strong>
                             <p>{user.bio}</p>
                         </footer>
                         
                         <div className="buttons">
-                            <button type="button">
+                            <button type="button" onClick={() => handleDislike(user._id)}>
                                 <img src={dislike} alt="dislike"/>
                             </button>
-                            <button type="button">
+                            <button type="button" onClick={() => handleLike(user._id)}>
                                 <img src={like} alt="like"/>
                             </button>
                         </div>
                     </li>
                 ))}
             </ul>
+            ) : (
+                <div className="empty"> Thats all buddy :(</div>
+            )}
         </div>
     
     )} 
